ec84c9bfb1672a2a1491ce2be2473e184e9132a3
Aznos <aznosreal@gmail.com>
2025-01-05 16:56:25 -0500

    Made it edible and give effects

--- src/main/java/com/aznos/coffee/item/ModItems.java
+++ src/main/java/com/aznos/coffee/item/ModItems.java
@@ -1,15 +1,24 @@
 package com.aznos.coffee.item;
 
 import com.aznos.coffee.Coffee;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final FoodComponent COFFEE_CUP_COMPONENT = new FoodComponent.Builder()
+            .alwaysEdible()
+            .snack()
+            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 20 * 300, 1, false, false, false), 1.0F)
+            .build();
+
     public static final Item COFFEE_CUP = registerItem(
             "coffee_cup",
-            new Item(new Item.Settings())
+            new Item(new Item.Settings().food(COFFEE_CUP_COMPONENT))
     );
 
     private static Item registerItem(String name, Item item) {
