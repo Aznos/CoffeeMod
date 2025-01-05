d56a0bd89ff5375b709d3c5e6d3be17dabf49dde
Aznos <aznosreal@gmail.com>
2025-01-05 16:02:48 -0500

    Setup

---
+++ src/main/java/com/aznos/coffee/Coffee.java
@@ -0,0 +1,10 @@
+package com.aznos.coffee;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Coffee implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/aznos/coffee/client/CoffeeClient.java
@@ -0,0 +1,10 @@
+package com.aznos.coffee.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoffeeClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
