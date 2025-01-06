aa1bb838415b9a4a8abde1b5c5f434e4fb3613a1
Aznos <aznosreal@gmail.com>
2025-01-06 18:01:01 -0500

    Custom drying rack outline shape

--- src/main/java/com/aznos/coffee/block/custom/DryingRackBlock.java
+++ src/main/java/com/aznos/coffee/block/custom/DryingRackBlock.java
@@ -3,13 +3,23 @@ package com.aznos.coffee.block.custom;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 
 public class DryingRackBlock extends Block {
     public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
+
+    private static final VoxelShape SHAPE = VoxelShapes.cuboid(
+            0.0, 0.0, 0.0,
+            1.0, 0.5, 1.0
+    );
 
     public DryingRackBlock(Settings settings) {
         super(settings);
@@ -24,5 +34,10 @@ public class DryingRackBlock extends Block {
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
         return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+    }
+
+    @Override
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
     }
 }
