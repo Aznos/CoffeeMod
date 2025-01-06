6f80c7210c9aa6b140dfab4550bb4f944aa50fda
Aznos <aznosreal@gmail.com>
2025-01-06 17:57:19 -0500

    Drying rack recipe

--- src/main/java/com/aznos/coffee/datagen/ModRecipeGenerator.java
+++ src/main/java/com/aznos/coffee/datagen/ModRecipeGenerator.java
@@ -1,10 +1,14 @@
 package com.aznos.coffee.datagen;
 
+import com.aznos.coffee.block.ModBlocks;
 import com.aznos.coffee.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.block.Blocks;
 import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 
@@ -21,5 +25,17 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
                 RecipeCategory.FOOD,
                 ModItems.RAW_COFFEE_BEAN
         ).input(ModItems.COFFEE_CHERRY).criterion(FabricRecipeProvider.hasItem(ModItems.COFFEE_CHERRY), FabricRecipeProvider.conditionsFromItem(ModItems.COFFEE_CHERRY)).offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.DRYING_RACK)
+                .pattern("   ")
+                .pattern("WSW")
+                .pattern("| |")
+                .input('W', Blocks.OAK_PLANKS)
+                .input('|', Items.STICK)
+                .input('S', Items.STRING)
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .criterion(hasItem(Items.STRING), conditionsFromItem(Items.STRING))
+                .criterion(hasItem(Blocks.OAK_PLANKS), conditionsFromItem(Blocks.OAK_PLANKS))
+                .offerTo(exporter);
     }
 }
