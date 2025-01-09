505ace340317a398df2cc80a8a394ddbe0f165cf
Aznos <aznosreal@gmail.com>
2025-01-09 15:59:25 -0500

    Redone brewer block entity for future inventory support

--- src/main/java/com/aznos/coffee/block/entity/custom/BrewerBlockEntity.java
+++ src/main/java/com/aznos/coffee/block/entity/custom/BrewerBlockEntity.java
@@ -1,6 +1,8 @@
 package com.aznos.coffee.block.entity.custom;
 
+import com.aznos.coffee.block.entity.ImplementedInventory;
 import com.aznos.coffee.block.entity.ModBlockEntities;
+import com.aznos.coffee.item.ModItems;
 import com.aznos.coffee.screen.custom.BrewerScreenHandler;
 import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerFactory;
 import net.minecraft.block.BlockState;
@@ -8,85 +10,74 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventories;
-import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.listener.ClientPlayPacketListener;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.screen.PropertyDelegate;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
-public class BrewerBlockEntity extends BlockEntity implements Inventory, ExtendedScreenHandlerFactory<BlockPos> {
-    private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(1, ItemStack.EMPTY);
+public class BrewerBlockEntity extends BlockEntity implements ExtendedScreenHandlerFactory<BlockPos>, ImplementedInventory {
+    private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(3, ItemStack.EMPTY);
+
+    private static final int INPUT_SLOT = 0;
+    private static final int OUTPUT_SLOT = 1;
+    private static final int FLUID_ITEM_SLOT = 2;
+
+    protected final PropertyDelegate propertyDelegate;
+    private int progress = 0;
+    private int maxProgress = 72;
+    private final int DEFAULT_MAX_PROGRESS = 72;
 
     public BrewerBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.BREWER_BE, pos, state);
-    }
-
-    @Override
-    public int size() {
-        return inventory.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        for(int i = 0; i < size(); i++) {
-            ItemStack stack = getStack(i);
-            if(!stack.isEmpty()) {
-                return false;
+        this.propertyDelegate = new PropertyDelegate() {
+            @Override
+            public int get(int index) {
+                return switch(index) {
+                    case 0 -> BrewerBlockEntity.this.progress;
+                    case 1 -> BrewerBlockEntity.this.maxProgress;
+                    default -> 0;
+                };
             }
-        }
 
-        return true;
-    }
+            @Override
+            public void set(int index, int value) {
+                switch(index) {
+                    case 0: BrewerBlockEntity.this.progress = value;
+                    case 1: BrewerBlockEntity.this.maxProgress = value;
+                }
+            }
 
-    @Override
-    public ItemStack getStack(int slot) {
-        markDirty();
-        return inventory.get(slot);
-    }
-
-    @Override
-    public ItemStack removeStack(int slot, int amount) {
-        markDirty();
-        ItemStack stack = inventory.get(slot);
-        stack.decrement(amount);
-        return inventory.set(slot, stack);
-    }
-
-    @Override
-    public ItemStack removeStack(int slot) {
-        markDirty();
-        return Inventories.removeStack(inventory, slot);
-    }
-
-    @Override
-    public void setStack(int slot, ItemStack stack) {
-        markDirty();
-        inventory.set(slot, stack.copyWithCount(1));
-    }
-
-    @Override
-    public boolean canPlayerUse(PlayerEntity player) {
-        return Inventory.canPlayerUse(this, player);
-    }
-
-    @Override
-    public void clear() {
-        inventory.clear();
+            @Override
+            public int size() {
+                return 2;
+            }
+        };
     }
 
     @Override
     protected void writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
         super.writeNbt(nbt, registryLookup);
         Inventories.writeNbt(nbt, inventory, registryLookup);
+
+        nbt.putInt("brewer.progress", progress);
+        nbt.putInt("brewer.max_progress", maxProgress);
     }
 
     @Override
     protected void readNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
+        progress = nbt.getInt("brewer.progress");
+        maxProgress = nbt.getInt("brewer.max_progress");
+
         super.readNbt(nbt, registryLookup);
         Inventories.readNbt(nbt, inventory, registryLookup);
     }
@@ -104,5 +95,66 @@ public class BrewerBlockEntity extends BlockEntity implements Inventory, Extende
     @Override
     public @Nullable ScreenHandler createMenu(int syncId, PlayerInventory playerInventory, PlayerEntity player) {
         return new BrewerScreenHandler(syncId, playerInventory, this.pos);
+    }
+
+    @Override
+    public DefaultedList<ItemStack> getItems() {
+        return inventory;
+    }
+
+    public void tick(World world, BlockPos pos, BlockState state) {
+        if(hasRecipe() && canInsertIntoOutputSlot()) {
+            increaseCraftingProgress();
+            markDirty(world, pos, state);
+
+            if(hasCraftingFinished()) {
+                craftItem();
+                resetProgress();
+            }
+        } else {
+            resetProgress();
+        }
+    }
+
+    private boolean hasRecipe() {
+        ItemStack input = new ItemStack(ModItems.COFFEE_POWDER);
+        ItemStack output = new ItemStack(ModItems.COFFEE_CUP);
+
+        return this.getStack(INPUT_SLOT).getItem() == input.getItem() && canInsertAmountIntoOutputSlot(output.getCount()) && canInsertItemIntoOutputSlot(output);
+    }
+
+    private boolean canInsertAmountIntoOutputSlot(int count) {
+        return this.getStack(OUTPUT_SLOT).getMaxCount() >= this.getStack(OUTPUT_SLOT).getCount() + count;
+    }
+
+    private boolean canInsertItemIntoOutputSlot(ItemStack output) {
+        return this.getStack(OUTPUT_SLOT).isEmpty() || this.getStack(OUTPUT_SLOT).getItem() == output.getItem();
+    }
+
+    private boolean canInsertIntoOutputSlot() {
+        return this.getStack(OUTPUT_SLOT).isEmpty() || this.getStack(OUTPUT_SLOT).getCount() < this.getStack(OUTPUT_SLOT).getMaxCount();
+    }
+
+    private void increaseCraftingProgress() {
+        this.progress++;
+    }
+
+    private boolean hasCraftingFinished() {
+        return this.progress >= this.maxProgress;
+    }
+
+    private void craftItem() {
+        this.removeStack(INPUT_SLOT, 1);
+        this.setStack(OUTPUT_SLOT, new ItemStack(ModItems.COFFEE_CUP, this.getStack(OUTPUT_SLOT).getCount() + 1));
+    }
+
+    private void resetProgress() {
+        this.progress = 0;
+        this.maxProgress = DEFAULT_MAX_PROGRESS;
+    }
+
+    @Override
+    public @Nullable Packet<ClientPlayPacketListener> toUpdatePacket() {
+        return BlockEntityUpdateS2CPacket.create(this);
     }
 }
